cd0556f2821d5d8ea1e1778366230b9dd4f83a83
veeMenTECH <129105328+veeMenTECH@users.noreply.github.com>
2023-04-03 22:08:19 +0000

    Add files via upload

---
+++ project2q1.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as sg
+import pyttsx3
+
+layout = [[sg.Text('Enter Text: '), sg.Input(key='-INPUT-')],
+          [sg.Column([[sg.Radio('Male Voice', 'voice', key='-MALE-', default=True), sg.Radio('Female Voice', 'voice', key='-FEMALE-')]], element_justification='c')],
+          [sg.Button('Speak'), sg.Button('Exit')]]
+
+window = sg.Window('Text to Speech App', layout)
+engine = pyttsx3.init()
+
+def speak_text_custom(text, voice_type):
+    voices = engine.getProperty('voices')
+    if voice_type == 'male':
+        engine.setProperty('voice', voices[0].id)
+    else:
+        engine.setProperty('voice', voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+    elif event == 'Speak':
+        text_to_speak = values['-INPUT-']
+        if values['-MALE-']:
+            speak_text_custom(text_to_speak, 'male')
+        else:
+            speak_text_custom(text_to_speak, 'female')
+
+window.close()
+
+  
---
+++ project2q2.py
@@ -0,0 +1,43 @@
+import PySimpleGUI as sg
+import qrcode as qrcode_lib
+import os
+
+def generate_qrcode(text):
+    qrcode = qrcode_lib.QRCode(version=1, box_size=10, border=4)
+    qrcode.add_data(text)
+    qrcode.make(fit=True)
+    qrcode_img = qrcode.make_image(fill_color="black", back_color="blue")
+    return qrcode_img
+
+def main():
+    sg.theme("DarkBlue")
+    layout = [
+        [sg.Text("Enter text to generate QR Code: ")],
+        [sg.InputText(key="text_input")],
+        [sg.Button("Generate"), sg.Exit()],
+        [sg.Image(key="qrcode_output")]
+    ]
+
+    window = sg.Window("QR Code Generator", layout)
+
+    while True:
+        event, values = window.read()
+        if event == "Generate":
+            text = values["text_input"]
+            try:
+                qrcode_img = generate_qrcode(text)
+                qrcode_filename = "qrcode.png"
+                qrcode_img.save(qrcode_filename)
+                window["qrcode_output"].update(filename=qrcode_filename)
+                os.remove(qrcode_filename)
+            except qrcode_lib.exceptions.DataOverflowError:
+                sg.popup_error("Text too long to generate QR code.")
+            except Exception as e:
+                sg.popup_error(f"Error generating QR code: {e}")
+        elif event == sg.WIN_CLOSED or event == "Exit":
+            break
+
+    window.close()
+
+if __name__ == "__main__":
+    main()
